429718b56cfc17e26d406f60bef2ca3f0b37ef46
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-04-15 16:39:16 +0200

    chore: add spring boot devtools for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 kotlin {
